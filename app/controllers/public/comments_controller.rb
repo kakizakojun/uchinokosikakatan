ef87c6655caa776aecbc0db182fe6ff3fed9323f
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-11-09 04:51:36 +0000

    [add]CommentAjax

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,15 +2,14 @@ class Public::CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save!
-    redirect_to post_path(post)
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = post.id
+    @comment.save
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
   end
 
   private
