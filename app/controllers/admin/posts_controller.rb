4bd3871467d67ce000c8a5cfda962726a7933008
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-11-13 08:37:00 +0000

    [add]Adminユーザー一覧

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,22 @@
 class Admin::PostsController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+
+    # Post.where(user_id: user.id).includes(:user).order("create_at DEST")
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, medias: [])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       resources :comments,  only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
   end
-    resources :users, only: [:show, :edit, :update]do
+    resources :users, only: [:show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
@@ -30,8 +30,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'top' => 'homes#top', as: 'top'
-    resources :users, only: [:index, :show, :edit, :update]
+    resources :users, only: [:index, :show, :edit, :update] do
     resources :posts, only: [:index, :show]
+  end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
