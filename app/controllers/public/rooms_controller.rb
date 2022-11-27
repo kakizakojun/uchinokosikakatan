beffb0bd20c8ac0ecc8a6ad83a4ee187ebad55da
JunKakizako <111491185+kakizakojun@users.noreply.github.com>
2022-11-27 17:07:34 +0000

    レイアウト

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -2,19 +2,24 @@ class Public::RoomsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    room = Room.create
-    current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
-    another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
-    redirect_to room_path(room)
+    if current_user.email != "guest@example.com"
+      room = Room.create
+      current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+      another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+      redirect_to room_path(room)
+    else
+      flash[:alert] = "メッセージをするには新規登録またはログインが必要です"
+      redirect_to mypage_path
+    end
   end
 
   def index
-    current_enties = current_user.entries
-    my_room_id = []
-    current_enties.each do |entry|
-      my_room_id << entry.room.id
-    end
-    @another_enties = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
+      current_enties = current_user.entries
+      my_room_id = []
+      current_enties.each do |entry|
+        my_room_id << entry.room.id
+      end
+      @another_enties = Entry.where(room_id: my_room_id).where.not(user_id: current_user.id)
   end
 
   def show
